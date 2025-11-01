1b9943e2477454c8b05a4a793274415c90fb1b41
Lesyk <lesuk93@yahoo.com>
2025-11-01 16:11:13 +0200

    Add folder w3schools

--- w3schools/main.py
+++ w3schools/main.py
@@ -1,1 +1,48 @@
-print('PyCharm')
+# global
+# If you use the global keyword, the variable belongs to the global scope:
+# Also, use the global keyword if you want to change a global variable inside a function.
+# def myfunc():
+#   global x
+#   x = "fantastic"
+
+
+# Built-in Data Types
+# Text Type:	    str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	    dict
+# Set Types:	    set, frozenset
+# Boolean Type:	    bool
+# Binary Types:	    bytes, bytearray, memoryview
+# None Type:	    NoneType
+
+
+# list: ["apple", "cherry"]
+# tuple: ("apple", "banana", "cherry")
+# dict: {"name" : "John", "age" : 36}
+# set: {"apple", "banana", "cherry"}
+# frozenset: frozenset({"apple", "banana", "cherry"})
+
+# List is ordered and changeable. Allows duplicate members.
+# Tuple is ordered and unchangeable. Allows duplicate members.
+# Set - is unordered, unchangeable, and unindexed. No duplicate members.
+# Dictionary - is ordered and changeable. No duplicate members.
+
+
+# Random Number
+# import random
+# print(random.randrange(1, 10))
+
+
+# F-String is the preferred way of formatting strings.
+# txt = f"My name is John, I am {age}"
+
+
+# False statements:
+# bool(False)
+# bool(None)
+# bool(0)
+# bool("")
+# bool(())
+# bool([])
+# bool({})
