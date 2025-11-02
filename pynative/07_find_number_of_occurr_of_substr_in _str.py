49200c7a2303c040c13dbe314063b006af498f6b
Lesyk <lesuk93@yahoo.com>
2025-11-02 13:33:02 +0200

    Add pynative seventh exercise

---
+++ pynative/07_find_number_of_occurr_of_substr_in _str.py
@@ -0,0 +1,7 @@
+# Exercise 7: Find the number of occurrences of a substring in a string
+# Write a Python code to find how often the substring “Emma”
+# appears in the given string.
+
+str_x = "Emma is good developer. Emma is a writer"
+
+print(f"Emma appeared {str_x.count('Emma')} times")
